6363f48eec3ba406b6844449ea913810a27365ed
ahmed <am2157678@gmail.com>
2024-08-06 13:12:04 +0300

    portfolio

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Hero from './components/Hero';
+import Bio from './components/Bio';
+import Skills from './components/Skills';
+import Education from './components/Education';
+import Contact from './components/Contact';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Hero />
+      <Bio />
+      <Skills />
+      <Education />
+      <Contact />
     </div>
   );
 }
